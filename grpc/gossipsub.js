507495f8617435e19fa9a092acdf34a527eaee51
Flav (Lotus) <flaviene.scheidt@uni.lu>
2021-08-20 16:50:09 +0200

    Adjusting the message

--- grpc/gossipsub.js
+++ grpc/gossipsub.js
@@ -167,7 +167,7 @@ function toLibP2P(call, callback) {
 	{ 
         //my_node.pubsub.publish(topic, call.request.message)
         // msg_to_brodcast = call.request.message   //JSON.stringify({msg:call.request.message.toString(), validator_key:call.request.validator_key.toString()})
-        msg_to_brodcast = JSON.stringify({msg:call.request.message.toString(), validator_key:ValidatorKey});
+        msg_to_brodcast = JSON.stringify({msg:call.request.message, validator_key:ValidatorKey});
         my_node.pubsub.publish(topic,msg_to_brodcast) //publish the whole msg + validator key
         console.log("GRPC-Server: Put on Gossipsub: " + ValidatorKey)
 	}
