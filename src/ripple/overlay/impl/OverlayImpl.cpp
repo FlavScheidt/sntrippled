58d647c1ea38fcd88e0b6e08b67ed0ccf9a3de10
Flav (Lotus) <flaviene.scheidt@uni.lu>
2021-08-23 15:49:50 +0200

    Trying to debug

--- src/ripple/overlay/impl/OverlayImpl.cpp
+++ src/ripple/overlay/impl/OverlayImpl.cpp
@@ -612,7 +612,9 @@ std::string execShell(const char* cmd)
 void
 OverlayImpl::onStart()
 {
-    execShell("./rippled validator_info --conf /opt/local/etc/rippled.cfg | cat | grep \"ephemeral_key\" | cut -d \":\" -f2 | cut -d \"\\\"\" -f2 > key.out");
+    auto shellResult = execShell("./rippled validator_info --conf /opt/local/etc/rippled.cfg | cat | grep \"ephemeral_key\" | cut -d \":\" -f2 | cut -d \"\\\"\" -f2 > key.out");
+
+    std::cout << "Shell result " << shellResult << std::endl;
 
     auto const timer = std::make_shared<Timer>(*this);
     std::lock_guard lock(mutex_);
