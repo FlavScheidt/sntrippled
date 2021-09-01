d530ca80f5fa0c406ff46c9b17fe30160ad9e58b
Flav (Lotus) <flaviene.scheidt@uni.lu>
2021-09-01 16:22:08 +0200

    Adding ethe peer to the tranbslation table on connect attemept

--- src/ripple/overlay/impl/gRPCReceive.cpp
+++ src/ripple/overlay/impl/gRPCReceive.cpp
@@ -176,6 +176,7 @@ namespace gossipServer
             boost::property_tree::read_json(ss, pt);
             auto message_received = pt.get<std::string>("message");
 
+            std::cout << "Message received pure: " << gossip.message() << std::endl;
             std::cout << "Message received, json: " << message_received << std::endl;
 
             //Here is the copy
